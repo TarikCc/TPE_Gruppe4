6e565fe56fc9dcf3b97a945c81f951d1b969c5be
mariellefeil <mariellefeil@141.19.158.73>
2016-04-13 14:47:36 +0200

    Paketname

---
+++ src/de/hs_mannheim/tpe/sose2016_gr04_uebung2/CrypterException.java
@@ -0,0 +1,10 @@
+package de.hs_mannheim.tpe.sose2016_gr04_uebung2;
+
+public class CrypterException extends Throwable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/de/hs_mannheim/tpe/sose2016_gr04_uebung2/CrypterFactory.java
@@ -0,0 +1,5 @@
+package de.hs_mannheim.tpe.sose2016_gr04_uebung2;
+
+public class CrypterFactory {
+
+}
