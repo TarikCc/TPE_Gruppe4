8dfa2a25358eb8a67676055700f323217145b0e9
mariellefeil <mariellefeil@141.19.153.114>
2016-04-18 17:13:01 +0200

    test

--- src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2/CrypterXOR.java
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2/CrypterXOR.java
@@ -27,24 +27,34 @@ public class CrypterXOR implements Crypter {
 
 	@Override
 	public char verschluesseln(char klartextZeichen) throws CrypterException {
-		int stelle = 0;
+		int verschluesseltesZeichen = 0;
 		for (int i = 0; i <= ABC.length; i++) {
 			if (klartextZeichen == ABC[i]) {
 				for (int n = 0; n <= ABC.length; i++) {
 					if (schluessel.charAt(0) == ABC[n]) {
-						stelle = i ^ n;
+					verschluesseltesZeichen = i ^ n;
 					}
 				}
 			}
 		}
 
-		return ABC[stelle];
+		return ABC[verschluesseltesZeichen];
 	}
 
 	@Override
 	public char entschluesseln(char cypherTextZeichen) throws CrypterException {
+		int entschluesseltesZeichen  = 0;
+		for (int i = 0; i <= ABC.length; i++) {
+			if (cypherTextZeichen == ABC[i]) {
+				for (int n = 0; n <= ABC.length; i++) {
+					if (schluessel.charAt(0) == ABC[n]) {
+						entschluesseltesZeichen = i ^ n;
+					}
+				}
+			}
+		}
 
-		return 0;
+		return ABC[entschluesseltesZeichen];
 	}
 
 }
--- src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2_test/CrypterCaesarTest.java
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2_test/CrypterCaesarTest.java
@@ -1,5 +1,24 @@
 package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2_test;
 
-public class CrypterCaesarTest {
+import org.junit.Test;
 
+import de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2.CrypterCaesar;
+import de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2.CrypterException;
+import de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2.CrypterFactory;
+import de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2.Crypter;
+import de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2.VerschluesselungsTyp;
+
+
+
+
+
+
+
+public class CrypterCaesarTest {
+	
+    @Test 
+	public void testCaesar() throws CrypterException {
+		Crypter caesar = CrypterFactoryCrypterCaesar(s.schluessel(ki, typ))
+		assertEquals("C", caesar.entschluesseln("U");
+	}
 }
