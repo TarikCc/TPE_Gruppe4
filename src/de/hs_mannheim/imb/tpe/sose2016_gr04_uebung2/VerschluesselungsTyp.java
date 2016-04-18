712ac3f5e3427b0cb8a6144cfd12ed7652d58be1
mariellefeil <mariellefeil@141.19.153.114>
2016-04-18 11:10:27 +0200

    Testklassen

---
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2/VerschluesselungsTyp.java
@@ -0,0 +1,7 @@
+package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2;
+
+public enum VerschluesselungsTyp {
+	
+	CAESAR, SUBSTITUTION, XOR;
+
+}
---
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung2_test/CrypterCaesarTest.java
@@ -0,0 +1,5 @@
+package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung2_test;
+
+public class CrypterCaesarTest {
+
+}
