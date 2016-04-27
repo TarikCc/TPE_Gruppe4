f35a85000b7adc1a8610772e77d8ace35333b563
tarik <tarik@141.19.179.59>
2016-04-27 15:31:56 +0200

    Member Konstruktor

--- src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung3/Member.java
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung3/Member.java
@@ -2,6 +2,19 @@ package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung3;
 
 public class Member {
 
-
+	final int MitgliederID;
+	private String Nachname; 
+	private String Vorname; 
+	private int Mitgliedschaft; 
 	
+	
+	
+	public Member (int MitgliederID , String Nachname , String Vorname , int Mitgliedschaft){
+		this.MitgliederID = MitgliederID;
+		this.Nachname = Nachname;
+		this.Vorname = Vorname; 
+		this.Mitgliedschaft = Mitgliedschaft;
+	}
+	
+
 }
