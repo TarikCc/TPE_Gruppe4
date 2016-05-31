ac88b7a7cafc8a71e51d1af034eb028c67ab893d
Quokkalinchen <fmlief.ff@googlemail.com>
2016-05-31 16:21:06 +0200

    Update Zug.java

--- src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung4/Zug.java
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung4/Zug.java
@@ -1,106 +1,109 @@
 package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung4;
 
 /**
- * Klasse Zug stellt die Züge die auf einer Strecke 
- * fahren da, als Threads
- * mit den Eigenschaften Name, Position; Strecke auf der er fährt 
- * und die tatsächliche Geschwindigkeit die in km/sec umgerechnet wird 
+ * Klasse Zug stellt die Züge die auf einer Strecke fahren da, als Threads mit
+ * den Eigenschaften Name, Position; Strecke auf der er fährt und die
+ * tatsächliche Geschwindigkeit die in km/sec umgerechnet wird
  */
-public class Zug implements Runnable{
+public class Zug implements Runnable {
 	private final char name;
 	private volatile int position;
 	private Strecke u;
 	private long tatges;
-	
+
 	/**
-	 * Konstruktor 
-	 * @param name , Kennung des Zuges
-	 * @param startPosi, Startposition auf der Strecke
-	 * @param speed, Geschwindigkeit von km/h in km/sec umgerechnet
-	 * @param u , Strecke auf der der Zug fährt
+	 * Konstruktor
+	 * 
+	 * @param name
+	 *            , Kennung des Zuges
+	 * @param startPosi,
+	 *            Startposition auf der Strecke
+	 * @param speed,
+	 *            Geschwindigkeit von km/h in km/sec umgerechnet
+	 * @param u
+	 *            , Strecke auf der der Zug fährt
 	 */
-	public Zug(char name, int startPosi, int speed, Strecke u){
+	public Zug(char name, int startPosi, int speed, Strecke u) {
 		this.name = name;
 		this.position = startPosi;
 		this.u = u;
 		tatges = (long) (1000 / speed);
 		u.getBlockList().get(blockIdPruefer()).setEinfahren();
 	}
-	
+
 	/**
-	 * überschriebene run()Methode des Interface von Runnable 
-	 * 
+	 * Methode sorgt dafür dass die Züge fahren und zur richtigen Zeit stehen
+	 * bleiben
 	 */
 	@Override
-	public synchronized void run()  {
-		while(position < u.getLaenge()){
+	public synchronized void run() {
+		while (position < u.getLaenge()) {
 			try {
 				Simulation.unfallpruefer(position, name);
 			} catch (SimulationException e1) {
 			}
-			if(position < u.getBlockList().get(blockIdPruefer()).getEndKm()){
-					try {
-						Thread.sleep(tatges);
-					} catch (InterruptedException e) {
-						break;
-					}
-					
-					position++;
-					System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
-			}
-			else{
-				if(blockIdPruefer() < u.getBlockList().size() && !u.getBlockList().get(blockIdPruefer()+1).getSignal()){
-					
+			if (position < u.getBlockList().get(blockIdPruefer()).getEndKm()) {
+				try {
+					Thread.sleep(tatges);
+				} catch (InterruptedException e) {
+					break;
 				}
-				else{
+
+				position++;
+				System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
+			} else {
+				if (blockIdPruefer() < u.getBlockList().size()
+						&& !u.getBlockList().get(blockIdPruefer() + 1).getSignal()) {
+
+				} else {
 					u.getBlockList().get(blockIdPruefer()).setAusfahren();
-					u.getBlockList().get(blockIdPruefer()+1).setEinfahren();
+					u.getBlockList().get(blockIdPruefer() + 1).setEinfahren();
 					position++;
 					System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
 				}
 			}
 		}
-		u.getBlockList().get(u.getBlockList().size()-1).setAusfahren();
+		u.getBlockList().get(u.getBlockList().size() - 1).setAusfahren();
 		System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
-		System.out.println("Zug "+name+" hat das ziel erreicht (Position: "+position+")");
+		System.out.println("Zug " + name + " hat das ziel erreicht (Position: " + position + ")");
 		position++;
 		Simulation.zuegeImZiel++;
 		Simulation.letzterZug();
 	}
 
-	
 	/**
 	 * Gibt den Index des Blocks zurück in dem sich der Zug aktuell befindet
+	 * 
 	 * @return d, Index des Blocks
 	 */
-	private int blockIdPruefer(){
+	private int blockIdPruefer() {
 		int d = 0;
 		for (Block b : u.getBlockList()) {
-			if(b.getAnfangKm() <= position && b.getEndKm() >= position){
+			if (b.getAnfangKm() <= position && b.getEndKm() >= position) {
 				break;
-			}
-			else{
+			} else {
 				d++;
 			}
 		}
 		return d;
 	}
-	
+
 	/**
+	 * Gibt Position des Zuges zurück
 	 * 
-	 * @return
+	 * @return position
 	 */
-	public int getPosition(){
+	public int getPosition() {
 		return position;
 	}
-	
+
 	/**
+	 * Gibt Name des Zuges zuück
 	 * 
-	 * @return
+	 * @return name
 	 */
-	public char getName(){
+	public char getName() {
 		return name;
 	}
 
 }
-
