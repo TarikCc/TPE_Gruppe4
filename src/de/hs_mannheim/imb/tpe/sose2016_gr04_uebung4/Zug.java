2ffb64f523f450718d1ead65de0d660e7dd66165
Quokkalinchen <fmlief.ff@googlemail.com>
2016-05-31 15:54:17 +0200

    Update Zug.java

--- src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung4/Zug.java
+++ src/de/hs_mannheim/imb/tpe/sose2016_gr04_uebung4/Zug.java
@@ -1,113 +1,106 @@
 package de.hs_mannheim.imb.tpe.sose2016_gr04_uebung4;
 
 /**
- * Klasse Zug, implentiert Interface Runnable da Züge mittels Threads realisiert
- * werden mit den Eigenschaften: name, position auf der Strecke, Geschwindigkeit
- * umgerechnet in km/s, Strecke auf der sich die Züge befinden und eine
- * Geschwindigkeitm
+ * Klasse Zug stellt die ZÃ¼ge die auf einer Strecke 
+ * fahren da, als Threads
+ * mit den Eigenschaften Name, Position; Strecke auf der er fÃ¤hrt 
+ * und die tatsÃ¤chliche Geschwindigkeit die in km/sec umgerechnet wird 
  */
-public class Zug implements Runnable {
+public class Zug implements Runnable{
 	private final char name;
 	private volatile int position;
 	private Strecke u;
 	private long tatges;
-
+	
 	/**
-	 * Konstruktor
-	 * 
-	 * @param name
-	 *            , Kennung des Zuges
-	 * @param startPosi,
-	 *            Startposition des Zuges auf der Strecke
-	 * @param speed,
-	 *            Geschwindigkeit in km/h
-	 * @param u,
-	 *            Strecke auf der sich der Zug befindet
+	 * Konstruktor 
+	 * @param name , Kennung des Zuges
+	 * @param startPosi, Startposition auf der Strecke
+	 * @param speed, Geschwindigkeit von km/h in km/sec umgerechnet
+	 * @param u , Strecke auf der der Zug fÃ¤hrt
 	 */
-	public Zug(char name, int startPosi, int speed, Strecke u) {
+	public Zug(char name, int startPosi, int speed, Strecke u){
 		this.name = name;
 		this.position = startPosi;
 		this.u = u;
 		tatges = (long) (1000 / speed);
 		u.getBlockList().get(blockIdPruefer()).setEinfahren();
 	}
-
+	
 	/**
+	 * Ã¼berschriebene run()Methode des Interface von Runnable 
 	 * 
-	 * run-Methode des Interface Runnable
 	 */
 	@Override
-	public synchronized void run() {
-		while (position < u.getLaenge()) {
-			if (position < u.getBlockList().get(blockIdPruefer()).getEndKm()) {
-				try {
-					Thread.sleep(tatges);
-				} catch (InterruptedException e) {
-
+	public synchronized void run()  {
+		while(position < u.getLaenge()){
+			try {
+				Simulation.unfallpruefer(position, name);
+			} catch (SimulationException e1) {
+			}
+			if(position < u.getBlockList().get(blockIdPruefer()).getEndKm()){
+					try {
+						Thread.sleep(tatges);
+					} catch (InterruptedException e) {
+						break;
+					}
+					
+					position++;
+					System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
+			}
+			else{
+				if(blockIdPruefer() < u.getBlockList().size() && !u.getBlockList().get(blockIdPruefer()+1).getSignal()){
+					
 				}
-				position++;
-				System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
-			} else {
-				if (blockIdPruefer() < u.getBlockList().size()
-						&& !u.getBlockList().get(blockIdPruefer() + 1).getSignal()) {
-
-				} else {
+				else{
 					u.getBlockList().get(blockIdPruefer()).setAusfahren();
-					u.getBlockList().get(blockIdPruefer() + 1).setEinfahren();
+					u.getBlockList().get(blockIdPruefer()+1).setEinfahren();
 					position++;
 					System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
 				}
 			}
 		}
-		u.getBlockList().get(u.getBlockList().size() - 1).setAusfahren();
+		u.getBlockList().get(u.getBlockList().size()-1).setAusfahren();
 		System.out.println(Simulation.printStrecke(u.getLaenge(), u.getBlockList()));
-		System.out.println("Zug " + name + " hat das ziel erreicht (Position: " + position + ")");
+		System.out.println("Zug "+name+" hat das ziel erreicht (Position: "+position+")");
 		position++;
+		Simulation.zuegeImZiel++;
+		Simulation.letzterZug();
 	}
 
+	
 	/**
-	 * Unfall-Methode
+	 * Gibt den Index des Blocks zurÃ¼ck in dem sich der Zug aktuell befindet
+	 * @return d, Index des Blocks
 	 */
-	private void unfall(int otherPosition) throws SimulationException {
-		if (this.position == otherPosition){
-			throw new SimulationException();
-		}
-
-	}
-
-	/**
-	 * Gibt den Index des Blocks zurück in dem sich der Zug aktuell befindet
-	 * 
-	 * @return d
-	 */
-	private int blockIdPruefer() {
+	private int blockIdPruefer(){
 		int d = 0;
 		for (Block b : u.getBlockList()) {
-			if (b.getAnfangKm() <= position && b.getEndKm() >= position) {
+			if(b.getAnfangKm() <= position && b.getEndKm() >= position){
 				break;
-			} else {
+			}
+			else{
 				d++;
 			}
 		}
 		return d;
 	}
-
+	
 	/**
-	 * Gibt die Postion des Zuges auf der Strecke zurück
 	 * 
-	 * @return position
+	 * @return
 	 */
-	public int getPosition() {
+	public int getPosition(){
 		return position;
 	}
-
+	
 	/**
-	 * Gibt den Namen des Zuges zurück
 	 * 
-	 * @return name
+	 * @return
 	 */
-	public char getName() {
+	public char getName(){
 		return name;
 	}
 
 }
+
